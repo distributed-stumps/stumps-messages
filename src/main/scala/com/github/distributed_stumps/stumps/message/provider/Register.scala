1552b10f4c2c26ddaf0c14c2ffdce64267de73a1
John Murray <me@johnmurray.io>
2014-12-29 02:37:32 -0500

    fixed namespaces in provider classes
    was still pointing to the old namespace, fixed

--- src/main/scala/com/github/distributed_stumps/stumps/message/provider/Register.scala
+++ src/main/scala/com/github/distributed_stumps/stumps/message/provider/Register.scala
@@ -1,6 +1,6 @@
 package com.github.distributed_stumps.stumps.message.provider
 
 import akka.actor.ActorRef
-import io.johnmurray.akkanamenode.message.common._
+import com.github.distributed_stumps.stumps.message.common._
 
 case class Register(host: Host, resource: Resource, heartBeatListener: ActorRef)
--- src/main/scala/com/github/distributed_stumps/stumps/message/provider/Unregister.scala
+++ src/main/scala/com/github/distributed_stumps/stumps/message/provider/Unregister.scala
@@ -1,7 +1,7 @@
 package com.github.distributed_stumps.stumps.message.provider
 
 import akka.actor.ActorRef
-import io.johnmurray.akkanamenode.message.common._
+import com.github.distributed_stumps.stumps.message.common._
 
 case class Unregister(host: Host, resource: Resource, heartBeatListener: ActorRef)
 
